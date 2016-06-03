fec7a214c33228d2c294df4eb7de9c15a72f6a8f
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-06-03 14:16:38 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//play a button, find out what computer played, compare
+//find an event of what i played, event of what computer played, //score
+	//define variables rock, paper, and scissors buttons
+​
+//find what user played
+//function human pick rock
+​
+		//then computer wins
+		//set status to you lost sentence
+​
+		//add 1 to computer score
+​
+	//function computer pick rock
+​
+		//then tie
+		//set status to you tie sentence
+​
+		//then computer loses
+		//set status to you win sentence
+
+		//add 1 to computer score
+​
+//function human pick paper
+​
+		//then computer wins
+		//set status to you lost sentence
+​
+		//add 1 to computer score
+​
+	//function computer pick paper
+​
+		//then tie
+		//set status to you tie sentence
+​
+		//then computer loses
+		//set status to you win sentence
+​
+		//add 1 to computer score
+​
+
+//function human pick scissors
+​
+
+		//then computer wins
+		//set status to you lost sentence
+
+		//add 1 to computer score
+​
+	//function computer pick paper
+
+		//then tie
+		//set status to you tie sentence
+​
+		//then computer loses
+		//set status to you win sentence
+​
+		//add 1 to computer score
+​
+//this function generates the computers play
+
+var app = angular.module('rps-app', []);
+
+app.controller('MainController', function($scope){
+
+});
